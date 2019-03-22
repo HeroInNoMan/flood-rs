13eef2d3eca501fafbe3c0366409872044c90462
Arthur Léothaud <arthur.leothaud@viseo.com>
2019-03-22 14:05:18 +0100

    add first functions to init the board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+
+#[derive(Clone)]
+enum Color {
+    RED,
+    YELLOW,
+    BLUE,
+    BLACK,
+}
+
+#[derive(Clone)]
+struct Cell {
+    is_flooded: bool,
+    color: Color,
+}
+
+impl Cell {
+    fn default() -> Self {
+        Cell {
+            is_flooded: false,
+            color: Color::BLACK,
+        }
+    }
+}
+
+struct FloodBoard {
+    board: Vec<Vec<Cell>>,
+}
+
+impl FloodBoard {
+    fn new(height: usize, width: usize) -> Self {
+        let mut flood_board = FloodBoard {
+            board: vec![vec![Cell::default(); width]; height],
+        };
+        flood_board.board[0][0].is_flooded = true;
+        flood_board
+    }
+}
+
+fn play(board: FloodBoard) {}
+
 fn main() {
-    println!("Hello, world!");
+    let board = FloodBoard::new(12, 12);
+    play(board);
 }
