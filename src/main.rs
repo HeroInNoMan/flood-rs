008e39403e3eecfc2f4a1e1111b4fb9ea2d09672
Arthur Léothaud <arthur.leothaud@viseo.com>
2019-03-22 14:30:31 +0100

    add colors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
+use colored::*;
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+use std::fmt::{Display, Formatter, Result};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Color {
     RED,
     YELLOW,
     BLUE,
-    BLACK,
+}
+
+impl Distribution<Color> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Color {
+        match rng.gen_range(0, 3) {
+            0 => Color::RED,
+            1 => Color::YELLOW,
+            _ => Color::BLUE,
+        }
+    }
+}
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let cell = match self {
+            Color::RED => "X".on_red(),
+            Color::YELLOW => "X".on_yellow(),
+            Color::BLUE => "X".on_blue(),
+        };
+        write!(f, "{}", cell)
+    }
 }
 
 #[derive(Clone)]
@@ -17,7 +42,7 @@ impl Cell {
     fn default() -> Self {
         Cell {
             is_flooded: false,
-            color: Color::BLACK,
+            color: Color::BLUE,
         }
     }
 }
@@ -31,14 +56,30 @@ impl FloodBoard {
         let mut flood_board = FloodBoard {
             board: vec![vec![Cell::default(); width]; height],
         };
+        for col in &mut flood_board.board {
+            for cell in col {
+                cell.color = rand::random::<Color>();
+            }
+        }
+
         flood_board.board[0][0].is_flooded = true;
         flood_board
+    }
+
+    fn display(&self) {
+        for col in &self.board {
+            for cell in col {
+                print!("{}", cell.color);
+            }
+            println!("");
+        }
     }
 }
 
 fn play(board: FloodBoard) {}
 
 fn main() {
-    let board = FloodBoard::new(12, 12);
+    let board = FloodBoard::new(3, 3);
+    board.display();
     play(board);
 }
